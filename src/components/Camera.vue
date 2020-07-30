fcf50136c9f9d5bd0efe0e87cca72d6d21af1e09
Jonathan Law <jon_law98@hotmail.com>
2020-07-30 16:31:58 +0800

    Fixed camera

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -2,26 +2,30 @@
     <v-card>
         <v-card-text>
             <v-row>
-                <v-col cols="12" style="position: relative">
-                    <video width="100%" id="videocam" autoplay></video>
+                <v-col cols="12" style="position: relative" v-if="devices.length !== 0">
+                    <video width="100%" id="videocam" autoplay ></video>
 <!--                            <img class="overlay" style="width: 90%" src="/img/image_framing.svg">-->
+                </v-col>
+
+                <v-col cols="12" class="mt-3" v-else>
+                    <h3 class="text-center">No camera detected!!</h3>
                 </v-col>
 
                 <v-col cols="12">
                     <v-row class="text-center" align="center">
-                        <v-col cols="4">
+                        <v-col cols="3">
                             <v-btn class="grey darken-4 side-buttons" outlined dark fab v-on:click="destroy">
-                                <v-icon style="height: 60px; font-size: 30px;">close</v-icon>
+                                <v-icon style="height: 32px; font-size: 24px;">close</v-icon>
                             </v-btn>
                         </v-col>
-                        <v-col cols="4">
+                        <v-col cols="6" v-if="devices.length !== 0">
                             <v-btn class="grey darken-4" style="height: 90px; width: 90px;" dark fab v-on:click="takePicture">
-                                <v-icon style="height: 90px; font-size: 110px;">adjust</v-icon>
+                                <v-icon style="height: 96px; font-size: 110px;">adjust</v-icon>
                             </v-btn>
                         </v-col>
-                        <v-col cols="4" v-if="this.devices.length > 1">
+                        <v-col cols="3" v-if="devices.length > 0">
                             <v-btn class="grey darken-4 side-buttons" outlined dark fab v-on:click="switchCamera">
-                                <v-icon style="height: 60px; font-size: 30px;">camera_front</v-icon>
+                                <v-icon style="height: 32px; font-size: 24px;">camera_front</v-icon>
                             </v-btn>
                         </v-col>
                     </v-row>
@@ -37,7 +41,7 @@
         data(){
             return {
                 devices : [],
-                vidConstraints : { video: true }
+                vidConstraints : { video: { facingMode : "environment"} },
             }
         },
         methods:{
@@ -51,8 +55,8 @@
             },
 
             stopStream(){
-                let tracks = document.querySelector('video').srcObject.getTracks();
-                tracks.forEach(track => track.stop())
+                (this.devices.length !== 0) &&
+                    document.querySelector('video').srcObject.getTracks().forEach(track => track.stop())
             },
 
             async getDevices(){
@@ -63,7 +67,7 @@
 
             switchCamera(){
                 if (this.devices.length > 1){
-                    this.vidConstraints.video.facingMode.exact = (this.vidConstraints.video.facingMode.exact === 'user' ) ? 'environment' : 'user';
+                    this.vidConstraints.video.facingMode = (this.vidConstraints.video.facingMode === "environment") ? "user" : "environment";
                     this.stopStream();
                     this.startStream();
                 }
@@ -86,12 +90,10 @@
                 this.$emit('capturedImage', null);
             }
         },
-        mounted() {
+        async mounted() {
             if('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
-                this.startStream();
-                this.getDevices();
-            } else {
-                alert("no camera");
+                await this.getDevices();
+                (this.devices.length !== 0) && this.startStream();
             }
         }
     }
@@ -106,7 +108,7 @@
     }
 
     .side-buttons{
-        height: 60px;
-        width: 60px;
+        height: 54px;
+        width: 54px;
     }
 </style>
